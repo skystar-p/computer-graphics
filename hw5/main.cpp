cf7e02329d70aa4f2c58d682a0686b29e249113d
skystar-p <wogus150@naver.com>
2018-06-08 23:47:37 +0900

    more beautify

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -37,9 +37,9 @@ int main() {
 
     Sphere sphere2(
             glm::vec3(0.0f, 0.0f, 50.0f), 50.0f,
-            glm::vec3(0.0f, 0.15f, 0.15f),
-            glm::vec3(0.2f, 0.2f, 0.2f),
-            glm::vec3(0.7f, 0.7f, 0.7f),
+            glm::vec3(0.1745f, 0.01175f, 0.01175f),
+            glm::vec3(0.61424f, 0.04136f, 0.04136f),
+            glm::vec3(0.727811f, 0.626959f, 0.626959f),
             3, 1.03f, true, true);
 
     (CAST(&sphere1))->texture = read_image((char *)"./assets/texture.png");
@@ -47,6 +47,17 @@ int main() {
 
     (CAST(&sphere2))->reflect_coeff = 1.5f;
     (CAST(&sphere2))->refract_coeff = 0.4f;
+
+    for (int i = 0; i < 11; i++) {
+        float yd = abs(5 - i) * 10.0f;
+        Sphere *ss = new Sphere(
+                glm::vec3(-200.0f + 40 * i, 120.0f - yd, 50.0f), 7.0f,
+                glm::vec3(0.01f, 0.01f, 0.01f),
+                glm::vec3(0.2f, 0.2f, 0.2f),
+                glm::vec3(0.8f, 0.8f, 0.8f),
+                3, 1.03f, true, true);
+        world.objects.push_back(CAST(ss));
+    }
 
     Triangle mirror1(
             glm::vec3(0.0f, 200.0f, -200.0f),
@@ -93,29 +104,32 @@ int main() {
             glm::vec3(400.0f, -70.0f, -400.0f),
             glm::vec3(-400.0f, -70.0f, -400.0f),
             glm::vec3(400.0f, -70.0f, 400.0f),
-            glm::vec3(0.5f, 0.0f, 0.0f),
+            glm::vec3(0.0f, 0.01f, 0.0f),
             glm::vec3(0.4f, 0.4f, 0.4f),
             glm::vec3(0.7f, 0.7f, 0.7f),
-            3, 1.0f, false, false);
+            3, 1.0f, true, false);
 
     Triangle floor2(
             glm::vec3(-400.0f, -70.0f, -400.0f),
             glm::vec3(-400.0f, -70.0f, 400.0f),
             glm::vec3(400.0f, -70.0f, 400.0f),
-            glm::vec3(0.5f, 0.0f, 0.0f),
+            glm::vec3(0.0f, 0.01f, 0.0f),
             glm::vec3(0.4f, 0.4f, 0.4f),
             glm::vec3(0.7f, 0.7f, 0.7f),
-            3, 1.0f, false, false);
+            3, 1.0f, true, false);
+
+    (CAST(&floor1))->reflect_coeff = 0.7f;
+    (CAST(&floor2))->reflect_coeff = 0.7f;
 
     Surface surface(
-            glm::vec3(0.0f, 0.0f, 0.5f),
-            glm::vec3(0.3f, 0.3f, 0.3f),
-            glm::vec3(0.4f, 0.4f, 0.4f),
+            glm::vec3(0.1f, 0.18725f, 0.1745f),
+            glm::vec3(0.396f, 0.74151f, 0.69102f),
+            glm::vec3(0.297254f, 0.30829f, 0.306678f),
             3, 1.0f, true, false);
 
     load_obj(surface, (char *)"./assets/octahedron.obj");
     surface.scale(30.0f);
-    surface.translate(glm::vec3(40.0f, -30.0f, 100.0f));
+    surface.translate(glm::vec3(80.0f, -30.0f, 100.0f));
 
     world.objects.push_back(CAST(&sphere1));
     world.objects.push_back(CAST(&sphere2));
